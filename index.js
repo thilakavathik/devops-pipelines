1c17cc25afd218d38b082ca459d9c9ae70d63d40
Thilakavathi <thilakavathik2005@gmail.com>
2026-07-18 11:52:54 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, DevOps!")
+console.log("This is my first Github action workflow.")
---
+++ tests.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...")
+timeout(() => {
+  console.log("Tests running...")
+}, 3000)
+console.log("Tests completed successfully!")
